01a51604c7b2c867d08f2a6b7607bc1c82da038d
blakeNaccarato <blake.naccarato@gmail.com>
2022-02-16 11:38:57 -0800

    Implement reading of a given property

--- examples/example.py
+++ examples/example.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import propshop
+from propshop import config
 from propshop.library import Mat, Prop
 
-propshop.get_thermal_conductivity(
-    Mat.COPPER, Prop.THERMAL_CONDUCTIVITY, pd.Series([300, 350, 400])
-)
+propshop.get_property(Mat.COPPER, Prop.THERMAL_CONDUCTIVITY, pd.Series([300, 350, 400]))
 ...
--- src/propshop/api.py
+++ src/propshop/api.py
@@ -6,9 +6,9 @@ from propshop.configs import config
 from propshop.library import Mat, Prop
 
 
-def get_thermal_conductivity(material: Mat, prop: Prop, temperatures: pd.Series):
-    get_property(material, prop)
-
-
-def get_property(material: Mat, prop: Prop):
-    df = pd.read_feather(config.tables / f"{material.name}.feather")
+def get_property(material: Mat, prop: Prop, temperatures: pd.Series):
+    df = pd.read_feather(
+        config.tables / f"{material.name}.feather",  # type: ignore
+        columns=["TEMPERATURE", prop.name],
+    )
+    ...
