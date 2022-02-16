0f12ba5f1b08f4852d5f39a5e9069304608e1f28
blakeNaccarato <blake.naccarato@gmail.com>
2022-02-16 09:22:32 -0800

    Bump template

--- tooling/setup.ps1
+++ tooling/setup.ps1
@@ -1,9 +1,4 @@
-copier -f -r 679fe01
+copier -f -r 9657048
 Remove-Item .venv -Recurse -ErrorAction SilentlyContinue
 py -3.10 -m venv .venv
-.venv/Scripts/activate
-pip install -U pip  # throws [WinError 5], but works
-pip install -U setuptools wheel  # must be done separately from above
-pip install -r tooling/requirements_dev.txt
-pip uninstall -y propshop
-pip install -e .
+. tooling/update.ps1
--- tooling/update.ps1
+++ tooling/update.ps1
@@ -1,4 +1,3 @@
-copier -f -r 679fe01
 .venv/Scripts/activate
 pip install -U pip  # throws [WinError 5], but still works on its own
 pip install -U setuptools wheel  # must be done separately from above
