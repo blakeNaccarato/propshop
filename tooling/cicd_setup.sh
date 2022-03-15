833c78ec7f08a0ff5ecabf2cce01d60c082a78b4
blakeNaccarato <blake.naccarato@gmail.com>
2022-03-14 20:18:19 -0700

    Bump template

--- tooling/cicd_setup.sh
+++ tooling/cicd_setup.sh
@@ -1,6 +1,6 @@
 pip install -U pip wheel
 pip install pipx
 pipx install copier==6.0.0a9
-copier -f -r 9657048
+copier -f -r 9087cd5
 pip install -r tooling/requirements_cicd.txt
 flit install
--- tooling/setup.ps1
+++ tooling/setup.ps1
@@ -1,4 +1,4 @@
-copier -f -r 9657048
+copier -f -r 9087cd5
 Remove-Item .venv -Recurse -ErrorAction SilentlyContinue
 py -3.10 -m venv .venv
 . tooling/update.ps1
