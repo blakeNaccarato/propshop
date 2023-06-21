f27d05b911f04fd646ef48a100ef28ef84361cb2
blakeNaccarato <blake.naccarato@gmail.com>
2023-06-21 14:42:38 -0700

    Update template digest to 1bbbc7c

--- .tools/scripts/Copy-Template.ps1
+++ .tools/scripts/Copy-Template.ps1
@@ -1,7 +1,23 @@
 <#.SYNOPSIS
-Update the project to the latest template version, ignoring project-specific deviations.
+Update the project from the project template.
 #>
 
-& '.tools/scripts/template_common.ps1'
-copier 'gh:blakeNaccarato/copier-python' '.'
+Param(
+    # Whether to recopy the template, ignoring prior diffs, or update in a smart manner.
+    [switch]$Recopy,
+
+    # Whether to use default values for unanswered questions.
+    [switch]$Defaults
+)
+
+if ( $Recopy ) {
+    copier recopy --force $(if ($Defaults) { '--defaults' })
+}
+else {
+    git submodule update --init --remote --merge template
+    git add --all
+    git commit -m "Update template digest to $(git rev-parse --short HEAD:template)"
+    git submodule deinit template
+    copier update $(if ($Defaults) { '--defaults' })
+}
 python '.tools/scripts/compose_pyproject.py'
